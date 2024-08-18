f8ac455d3e93b759ba00bdf97c9de4791daae25e
hassan95eb <19h.amini95@gmail.com>
2024-08-18 13:13:46 +0330

    define function that  management user action

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
   const [title, setTitle] = useState("");
   const [postId, setPostId] = useState(1);
   const [loading, setLoading] = useState(true);
-  return <div>App</div>;
+  function userAction(type, playLoad) {
+    switch (type) {
+      case "get-post-id":
+        setTitle(playLoad);
+        setLoading(false);
+        break;
+      case "change-post-id":
+        setLoading(true);
+        setPostId(playLoad);
+        break;
+      default:
+        break;
+    }
+  }
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((response) => response.json())
+      .then((post) => {
+        userAction("get-post-id", post.title);
+      });
+  }, [postId]);
+  function loadingFunc(e) {
+    userAction("change-post-id", e.target.value);
+  }
+  return (
+    <>
+      <div>
+        <label htmlFor="">post Id:</label>
+        <input type="number" onChange={loadingFunc} value={postId} />
+      </div>
+      <div className="d-flex justify-content-center align-items-center">
+        {loading ? (
+          <div class="spinner-border text-primary" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+        ) : (
+          <div> {title}</div>
+        )}
+      </div>
+    </>
+  );
 }
